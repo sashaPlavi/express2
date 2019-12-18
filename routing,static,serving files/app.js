07cd62b2278b13282018fee4bc84d29d31f4494a
SashaPlavi <sasha.bingulac@gmail.com>
2019-12-18 18:52:13 +0100

    conecting to mysql

--- routing,static,serving files/app.js
+++ routing,static,serving files/app.js
@@ -2,12 +2,20 @@ const express = require("express");
 const path = require("path");
 const rootdir = require("./util/path");
 const bodyparser = require("body-parser");
+const db = require("./util/database");
 
 const app = express();
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 app.use(express.static(path.join(rootdir, "public")));
 app.use(bodyparser.urlencoded({ extended: false }));
+db.execute("SELECT * FROM sys.products")
+  .then(res => {
+    console.log(res);
+  })
+  .catch(err => {
+    console.log(err);
+  });
 
 app.use(adminRoutes);
 app.use(shopRoutes);
